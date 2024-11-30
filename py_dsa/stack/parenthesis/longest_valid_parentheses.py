6a046aeed954d5fc8283bec3d43e655197d25cc5
wskraymond <starry2011night@gmail.com>
2024-11-30 16:45:13 +0800

    udpate

--- py_dsa/stack/parenthesis/longest_valid_parentheses.py
+++ py_dsa/stack/parenthesis/longest_valid_parentheses.py
@@ -8,20 +8,29 @@ class Solution:
                 0 <= s.length <= 3 * 104
                 s[i] is '(', or ')'.
         '''
+        '''
+            normal case 1: ()())()
+            normal case 2: (())()
+
+            edge case 1: )()()(()()))
+            edge case 2: )(
+
+            idea: 
+                we don't know if a open bracket is valid or not until an invalid close bracket exists or end of line
+            solution: 
+                by storing index of unmatched bracket or -1 , we're able compute the length of longest valid parnetheses.
+        '''
         stack = []
-        n = len(s)
-        res,count = 0,0
-        for c in s:
-            if c==')':
-                if stack:
-                   stack.pop()
-                   count+=2
-                   res = max(res, count)
-                else:
-                   count=0
+        res = 0
+        
+        for i,c in enumerate(s):
+            if stack and c==')':
+                top = stack.pop()
+                if s[top] == '(':
+                    last_index = stack[-1] if stack else -1
+                    res = max(res, i-last_index)
+                else: # new invalid bracket comes in
+                    stack.append(i)
             else:
-                stack.append 
-                 
-
-
-        pass
+                stack.append(i)
+        return res            
