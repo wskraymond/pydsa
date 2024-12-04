23bcc6e0dbecc1750f100ba47c382dc01d92e3cf
wskraymond <starry2011night@gmail.com>
2024-12-04 21:39:28 +0800

    update

--- py_dsa/stack/monotonic/daily_temperatures.py
+++ py_dsa/stack/monotonic/daily_temperatures.py
@@ -9,5 +9,24 @@ class Solution:
         If there is no future day for which this is possible, keep answer[i] == 0 instead.
     '''
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
-        pass
+        '''
+            Insertion-based:
+                0. A decreasing monotonic stack from right to left 
+                1. x = temperature[i] = inserted element
+                2. poping elements from stack till > x
+                3. answer[i] = top element index - i = the number of days to get warmer
+                4. if stack is empty, answer[i] = 0
+        '''
+
+        stack, n = [], len(temperatures)
+        ans = [0]*n
+        #for i,x in enumerate(reversed(temperatures)): #reverse value but not index
+        for i in reversed(range(n)):
+            x = temperatures[i]
+            while stack and temperatures[stack[-1]] <= x:
+                stack.pop()
+
+            ans[i] = stack[-1] - i if stack else 0
+            stack.append(i)
+                
+        return ans
